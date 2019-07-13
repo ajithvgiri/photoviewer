4a6fd7559b0a2800161e664f962c041407716f9d
ajithvgiri <ajithvgiri@qkopy.com>
2019-07-14 03:10:49 +0530

    completed commit

--- app/src/main/java/com/ajithvgiri/photoviewer/MainActivity.kt
+++ app/src/main/java/com/ajithvgiri/photoviewer/MainActivity.kt
@@ -2,11 +2,13 @@ package com.ajithvgiri.photoviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ajithvgiri.photoviewerlib.PhotoView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
